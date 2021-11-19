f526068ac9ead749e948fdcd631475699a84b007
unknown <davidpromntorfarias@gmail.com>
2021-11-19 15:44:26 -0600

    nuevos ajustes

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const port = process.env.PORT || '4000';
 const host = process.env.HOST || '0.0.0.0'
 
 
-app.listen(port, ()=>{  
+app.listen(port,host, ()=>{  
     
     console.log('El servidor esta funcionando en el puerto' + port);
 });
